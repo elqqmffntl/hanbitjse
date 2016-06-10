2c45683d55cabe4512d6de5aa8c6a20af78bdbcc
USER <USER@hb1030>
2016-06-10 09:07:57 +0900

    .

---
+++ hello/src/branch/Num1.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package branch;
+
+import java.util.Scanner;
+
+/**
+ * @date   : 2016. 6. 9. 
+ * @author : 한상호
+ * @file   : Num1.java
+ * @story  : 
+*/
+public class Num1 {
+    /**
+     * 저희 반은 학생수가 3명입니다.
+     * 학생의 평균점수는 0~100점 사이입니다.
+     * 3명의 평균점수를 입력하면
+     * 1등 홍길동
+     * 2등 김유신
+     * 3등 김구
+     * 이렇게 나오도록 해주세요. 동점은 없습니다.
+     * 단, 학생이름은 스캐너로 입력받습니다.
+     * 홍길동
+     * 49
+     * 김유신
+     * 78
+     * 김구
+     * 96
+     * ================
+     * 1등 김구 96점
+     * 2등 김유신 78점
+     * 3등 홍길동 49점
+     * ================
+     */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int H=0,K=0,Ku=0;
+		System.out.println("3명의 평균 점수를 입력하세요");
+	}
+}
